15663978f4a614702e982bd12a22d59ecb04f6ba
DawidHr <dawiid21@gmail.com>
2024-06-26 11:31:33 +0200

    git init

---
+++ src/main/java/com/dawidhr/ComanyGamesRestApi/ComanyGamesRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.dawidhr.ComanyGamesRestApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComanyGamesRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComanyGamesRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dawidhr/ComanyGamesRestApi/ComanyGamesRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dawidhr.ComanyGamesRestApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComanyGamesRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
